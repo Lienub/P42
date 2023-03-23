b1e9ace343146e8a2e38cf86a20b2d32afc9178f
Tristan Durieux <tdurieux@unistra.fr>
2023-03-23 13:38:45 +0100

    Début de l'implémentation du recyclerView

--- Projet/app/src/main/java/com/example/projet/ui/home/HomeViewModel.java
+++ Projet/app/src/main/java/com/example/projet/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Voici la liste de contacts");
     }
 
     public LiveData<String> getText() {
